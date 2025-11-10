f0090d6ba13f2186c38fffaf4b2eba0d457b387e
krajo155 <krajo155spam@gmail.com>
2025-11-10 09:15:20 +0100

    Your commit message here

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "project-composer-476710"
+  region  = "europe-west4"
+  credentials = jsondecode(env("GCP_CREDENTIALS"))
+}
+
+resource "google_storage_bucket" "poc_bucket" {
+  name     = "poc-test-bucket-${random_id.suffix.hex}"
+  location = "europe-west4"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
